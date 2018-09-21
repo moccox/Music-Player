5ea5745585dc954d0cb7e0d8fc1ae05ef4ebca72
Cx.Mo <3083669316@qq.com>
2018-09-21 10:44:09 +0800

    第三次提交，增加了音乐播放功能，增加了播放页面、主题颜色设置页面面

---
+++ app/src/main/java/com/example/administrator/music_player/myApplication/ChooseColor.java
@@ -0,0 +1,141 @@
+package com.example.administrator.music_player.myApplication;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.administrator.music_player.R;
+
+public class ChooseColor extends Activity{
+
+    private View mview1;
+    private View mview2;
+    private View mview3;
+    private View mview4;
+    private View mview5;
+    private View mview6;
+    private View mview7;
+    private View mview8;
+    private View mview9;
+    private Button msure;
+    private Button munSure;
+    private int mcolor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_color);
+        findView();
+        initListener();
+    }
+
+    /**关联相关组件**/
+    private void findView(){
+        mview1 = findViewById(R.id.v1);
+        mview2 = findViewById(R.id.v2);
+        mview3 = findViewById(R.id.v3);
+        mview4 = findViewById(R.id.v4);
+        mview5 = findViewById(R.id.v5);
+        mview6 = findViewById(R.id.v6);
+        mview7 = findViewById(R.id.v7);
+        mview8 = findViewById(R.id.v8);
+        mview9 = findViewById(R.id.v9);
+        msure = (Button) findViewById(R.id.sure);
+        munSure = (Button) findViewById(R.id.unsure);
+    }
+
+    /**按钮监听**/
+    private void initListener(){
+        msure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                if(mcolor != 0){
+                    intent.putExtra("color",mcolor);
+                    setResult(1,intent);
+                }
+                else setResult(2,intent);
+                finish();
+            }
+        });
+
+        munSure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcolor = 0;
+            }
+        });
+
+        /**取色**/
+
+        mview1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcolor = ChooseColor.this.getResources().getColor(R.color.black);
+            }
+        });
+
+        mview2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcolor = ChooseColor.this.getResources().getColor(R.color.gray);
+            }
+        });
+
+        mview3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcolor = ChooseColor.this.getResources().getColor(R.color.red);
+            }
+        });
+
+        mview4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcolor = ChooseColor.this.getResources().getColor(R.color.green);
+            }
+        });
+
+        mview5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcolor = ChooseColor.this.getResources().getColor(R.color.deepBlue);
+            }
+        });
+
+        mview6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcolor = ChooseColor.this.getResources().getColor(R.color.lightBlue);
+            }
+        });
+
+        mview7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcolor = ChooseColor.this.getResources().getColor(R.color.orange);
+            }
+        });
+
+        mview8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcolor = ChooseColor.this.getResources().getColor(R.color.pink);
+            }
+        });
+
+        mview9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcolor = ChooseColor.this.getResources().getColor(R.color.purple);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/administrator/music_player/myApplication/IndexActivity.java
+++ app/src/main/java/com/example/administrator/music_player/myApplication/IndexActivity.java
@@ -5,14 +5,16 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.administrator.music_player.R;
@@ -25,103 +27,117 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends Activity {
+/**
+ * 首页
+ * 包括：音乐列表、播放模式（未完善）、播放/暂停按钮、播放音乐信息
+ */
 
-    private ImageButton mpreviousBt;
-    private ImageButton mplayOrPauseBt;
-    private ImageButton mstopBt;
-    private ImageButton mnextBt;
-    private ListView mlist;
+public class IndexActivity extends Activity {
+    //各个界面返回值
+    private final int GO_TO_MAIN = 1; //界面1（MainActivity）返回值
+    private final int GO_TO_COLOR = 2;//界面2（ChooseColor）返回值
+
+    private ImageButton mplayOrPause;   //播放/暂停按钮
+    private ImageButton msettingBt;     //设置按钮
+    private ListView mlistView; //歌曲列表
+    private TextView mtitle;    //正在播放的歌曲标题
+    private TextView martist;   //正在播放的歌曲演唱者
 
     private ArrayList<Music> mmusicArrayList;           //装Music类的数组列表
     private MediaPlayer mplayer = new MediaPlayer();    // 媒体播放类
-    private int mmusicId = 0;   //记录播放歌曲的序号，初始化为0
-    private int mmusicPosition; //音乐播放点，记录歌曲播放进度
+    private int mmusicId = 0; //记录播放歌曲的序号，初始化为0
+    private boolean flag = true;
+    private ListView msettingList; //关于设置的具体菜单
+    private int mcolor; //主题颜色
+    private LinearLayout mplayingBar; //播放栏
+    private RelativeLayout msettingBar; //设置栏
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_index);
         findViews();
-        initListener();
         initMusicList();
         initListView();
         checkMusicfile();
-        initPosition();
+        initSettingList();
+        initListener();
     }
 
     /**组件关联**/
     private void findViews(){
-        mpreviousBt = (ImageButton) findViewById(R.id.previousButton);
-        mplayOrPauseBt = (ImageButton) findViewById(R.id.playButton);
-        mstopBt = (ImageButton) findViewById(R.id.stopButton);
-        mnextBt = (ImageButton) findViewById(R.id.nextButton);
-        mlist = (ListView) findViewById(R.id.myListView);
+        mplayOrPause = (ImageButton) findViewById(R.id.index_playOrPause);
+        mlistView = (ListView) findViewById(R.id.index_listView);
+        mtitle = (TextView) findViewById(R.id.index_title);
+        mtitle.setSelected(true);   //View太多获取不到焦点，不设置这个不会滚动
+        martist = (TextView) findViewById(R.id.index_artist);
+        martist.setSelected(true);
+        msettingBt = (ImageButton) findViewById(R.id.setting); //设置按钮
+        msettingList = (ListView) findViewById(R.id.setting_listView); //设置菜单
+        mplayingBar = (LinearLayout) findViewById(R.id.playingBar);
+        msettingBar = (RelativeLayout) findViewById(R.id.settingBar);
     }
 
     /**设置按钮监听**/
     private void initListener(){
-        mpreviousBt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /**播放上一首歌曲**/
-                if(mmusicId == 0){
-                    Toast.makeText(MainActivity.this, "已是第一首歌曲", Toast.LENGTH_LONG).show();
-                }else {
-                    --mmusicId;
-                    play(mmusicId);
-                }
-            }
-        });
 
-        mplayOrPauseBt.setOnClickListener(new View.OnClickListener() {
+        mplayOrPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /**播放或者暂停**/
+
                 if(mplayer != null && mplayer.isPlaying()){ //播放改暂停
                     pause();
-                    mplayOrPauseBt.setBackgroundResource(R.drawable.button_pause);  //按钮外观改成暂停
+                    mplayOrPause.setBackgroundResource(R.drawable.index_button_pause);  //按钮外观改成暂停
                 }else{
-                    if(mmusicId == 0){
+                    if(flag == true){
                         //一开始的播放
                         play(mmusicId);
+                        flag = false;
                     }else{
                         //暂停改播放
                         resume();
-                        mplayOrPauseBt.setBackgroundResource(R.drawable.button_play);   //按钮外观改回播放
+                        mplayOrPause.setBackgroundResource(R.drawable.index_button_play);   //按钮外观改回播放
                     }
                 }
             }
         });
 
-        mstopBt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /**停止播放**/
-                stop();
-            }
-        });
-
-        mnextBt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /**播放下一首歌曲**/
-                if(mmusicId == (MusicList.getMusicList().size()-1)){
-                    Toast.makeText(MainActivity.this, "已是最后一首歌曲", Toast.LENGTH_LONG).show();
-                }else{
-                    ++mmusicId;
-                    play(mmusicId);
-                }
-            }
-        });
-
-        mlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 /**单击该项播放该项**/
                 mmusicId = position;
                 play(mmusicId);
-                mplayOrPauseBt.setBackgroundResource(R.drawable.button_play);   //按钮外观改回播放
+            }
+        });
+
+        msettingBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**单击 弹开/收起 设置列表**/
+                if(msettingList. getVisibility() == View.GONE || msettingList.getVisibility() == View.INVISIBLE){
+                    //隐藏改显示
+                    msettingList.setVisibility(View.VISIBLE);
+                }else{
+                    //显示改隐藏
+                    msettingList.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        msettingList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){
+                    //设置主题颜色
+                    Intent intent = new Intent();
+                    intent.setAction("com.example.administrator.music_player.action.Color");
+                    intent.addCategory("android.intent.category.DEFAULT");
+                    startActivityForResult(intent,GO_TO_COLOR);
+                    pause();
+                }
             }
         });
     }
@@ -193,48 +209,40 @@ public class MainActivity extends Activity {
         String[] from = new String[]{"musicName","musicArtist"};
         int[] to = {R.id.itemTitle,R.id.itemArtist};
         simpleAapter = new SimpleAdapter(this,listMap,R.layout.listview_item,from,to);  //数组列表写入适配器
-        mlist.setAdapter(simpleAapter);     //写入ListView
+        mlistView.setAdapter(simpleAapter);     //写入ListView
+    }
+
+    /**初始化设置列表**/
+    private void initSettingList(){
+        /**往设置列表添加item**/
+       HashMap<String,String> map = new HashMap<String,String>();
+        List<Map<String,String>> listMap = new ArrayList<>();
+        map.put("Setting","主题颜色");
+        listMap.add(map);
+        String TAG = "initSettingList()";
+        map = new HashMap<String,String>();
+        map.put("Setting","均衡器");
+        listMap.add(map);
+        String[] from =new String[]{"Setting"};
+        int[] to = {R.id.setting_item};
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,listMap,R.layout.settinglist_item,from,to);
+        msettingList.setAdapter(simpleAdapter);
     }
 
     /**检查MusicList是否有歌曲**/
     private void checkMusicfile(){
         if(mmusicArrayList.isEmpty()){  //MusicList中没有Music
-            //各按钮设置不可用
-            mnextBt.setEnabled(false);
-            mplayOrPauseBt.setEnabled(false);
-            mpreviousBt.setEnabled(false);
-            mstopBt.setEnabled(false);
+
+            mplayOrPause.setEnabled(false);
+            mtitle.setText("");
+            martist.setText("");
 
             //提示没有歌曲
             Toast.makeText(this, "没有歌曲文件", Toast.LENGTH_LONG).show();
         }else{  //MusicList中有Music
             //不管之前有没有设置为不可用，都将按钮重新设置为可用
-            mnextBt.setEnabled(true);
-            mplayOrPauseBt.setEnabled(true);
-            mstopBt.setEnabled(true);
-            mpreviousBt.setEnabled(true);
+            mplayOrPause.setEnabled(true);
         }
-    }
-
-    /**初始化歌曲播放进度**/
-    private void initPosition(){
-        Intent intent = getIntent();
-        mmusicId = intent.getIntExtra("id",0);
-        mmusicPosition = intent.getIntExtra("position",0);
-        play(mmusicId);
-        mplayer.seekTo(mmusicPosition);
-    }
-
-    /**按下返回键，返回首页**/
-    @Override
-    public void onBackPressed(){
-        Intent intent = new Intent();
-        intent.putExtra("id",mmusicId);
-        mmusicPosition = mplayer.getCurrentPosition();
-        intent.putExtra("position",mmusicPosition);
-        setResult(1,intent);
-        mplayer.stop();
-        finish();
     }
 
     /**读取音乐文件（文件序号：number）**/
@@ -263,6 +271,12 @@ public class MainActivity extends Activity {
         Log.i(TAG, "play: 读取音乐文件成功");
         mplayer.start();
         Log.i(TAG, "play: 播放音乐，id："+mmusicId);
+
+        //将标题和演唱者写入
+        String title = mmusicArrayList.get(mmusicId).getMmusicName().toString().trim();
+        mtitle.setText(title);
+        String artist = mmusicArrayList.get(mmusicId).getMmusicArtist().toString().trim();
+        martist.setText(artist);
     }
 
     /**暂停播放**/
@@ -275,14 +289,53 @@ public class MainActivity extends Activity {
         mplayer.start();
     }
 
-    /**停止播放**/
-    private void stop(){
-        mplayer.stop();
-        mmusicId = 0;
-    }
-
     /**重新播放本歌曲（播放完成后）**/
     private void replay(){
         mplayer.start();
+    }
+
+    /**界面跳转1：跳转到MainActivity**/
+    public void goToMain(View view) {
+        Intent intent = new Intent();
+        intent.setAction("com.example.administrator.music_player.action.Main");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.putExtra("id",mmusicId);
+        int position = mplayer.getCurrentPosition();    //获取播放点
+        intent.putExtra("position",position);
+        mplayer.stop();
+        startActivityForResult(intent,GO_TO_MAIN);
+    }
+
+    /**回调结果处理**/
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        if(requestCode == GO_TO_MAIN){   //从播放界面返回
+            mmusicId = data.getIntExtra("id",mmusicId);
+            int position = data.getIntExtra("position",0);
+            play(mmusicId);
+            mplayer.seekTo(position);
+        }
+
+        if(requestCode == GO_TO_COLOR){    //从选色界面返回
+            if(resultCode == 1){
+                //有东西
+                mcolor = data.getIntExtra("color",mcolor);
+                String TAG = "result1";
+                Log.i(TAG, "onActivityResult: "+mcolor);
+                mplayingBar.setBackgroundColor(mcolor);
+                msettingBar.setBackgroundColor(mcolor);
+                msettingList.setBackgroundColor(mcolor);
+                changeColorOfList(msettingList,mcolor);
+            }else{}
+            resume();
+        }
+    }
+
+    /**改变设置选项菜单颜色**/
+    private void changeColorOfList(ListView listview,int color){
+        for(int position=0;position<listview.getChildCount();++position){
+            //获取每一个item，先关联（没有关联不会改变颜色），再改变背景颜色
+            listview.getChildAt(position).findViewById(R.id.setting_item).setBackgroundColor(color);
+        }
     }
 }
